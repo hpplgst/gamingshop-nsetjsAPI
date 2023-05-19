4a7f800f18c8ed6c55656072ab73537e5bfe3c5c
hpplgst <125792439+hpplgst@users.noreply.github.com>
2023-05-19 17:00:03 +0330

    Update role.guard.ts

--- src/auth/roles/role.guard.ts
+++ src/auth/roles/role.guard.ts
@@ -11,7 +11,6 @@ export class RoleGuards implements CanActivate {
         const { user } = context.switchToHttp().getRequest()
         const methodRoles = this.reflector.get("Roles", context.getHandler()) ? this.reflector.get("Roles", context.getHandler()) : []
         const controllerRoles = this.reflector.get("Roles", context.getClass()) ?  this.reflector.get("Roles", context.getClass()) : []
-        console.log(user)
         const roles = [...methodRoles, ...controllerRoles]
         return roles.some(r =>  r === user.role )
     }
